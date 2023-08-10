09260eff21f9040d37e2fc21bec1aad870aa7517
AmreshTripathy <amreshtripathy184@gmail.com>
2023-08-11 01:11:11 +0530

    Done

---
+++ main.js
@@ -0,0 +1,74 @@
+function leftScroll() {
+    document.getElementById('container').scrollBy({
+        left: Math.max(-1070),
+        behavior: 'smooth'
+    });
+}
+
+function rightScroll() {
+    document.getElementById('container').scrollBy({
+        left: Math.max(1070),
+        behavior: 'smooth'
+    });
+}
+
+
+
+function leftScroll_1() {
+    document.getElementById('container_1').scrollBy({
+        left: Math.max(-1070),
+        behavior: 'smooth'
+    });
+}
+
+function rightScroll_1() {
+    document.getElementById('container_1').scrollBy({
+        left: Math.max(1070),
+        behavior: 'smooth'
+    });
+}
+
+
+
+function leftScroll_2() {
+    document.getElementById('container_2').scrollBy({
+        left: Math.max(-1070),
+        behavior: 'smooth'
+    });
+}
+
+function rightScroll_2() {
+    document.getElementById('container_2').scrollBy({
+        left: Math.max(1070),
+        behavior: 'smooth'
+    });
+}
+
+
+function leftScroll_3() {
+    document.getElementById('container_3').scrollBy({
+        left: Math.max(-1070),
+        behavior: 'smooth'
+    });
+}
+
+function rightScroll_3() {
+    document.getElementById('container_3').scrollBy({
+        left: Math.max(1070),
+        behavior: 'smooth'
+    });
+}
+
+function leftScroll_4() {
+    document.getElementById('container_4').scrollBy({
+        left: Math.max(-1070),
+        behavior: 'smooth'
+    });
+}
+
+function rightScroll_4() {
+    document.getElementById('container_4').scrollBy({
+        left: Math.max(1070),
+        behavior: 'smooth'
+    });
+}
